303c82c4fdcd59a1f346dc7ea1794d5d7d31ea98
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2020-11-17 17:34:12 +0100

    Fixes setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools_rust import Binding, RustExtension
 setup(
     name="ldk-python",
     version="1.0",
-    rust_extensions=[RustExtension("ldk_python.primitives", binding=Binding.PyO3)],
+    rust_extensions=[RustExtension("ldk_python.ldk_python", binding=Binding.PyO3)],
     packages=["ldk_python"],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
