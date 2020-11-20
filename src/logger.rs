69ba4c0479eabf8f6eae67e9f6e3af6bd3bbf04e
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2020-11-20 15:23:47 +0100

    Improves logger

--- src/logger.rs
+++ src/logger.rs
@@ -1,9 +1,11 @@
 
 use pyo3::prelude::*;
 
-use lightning::util::logger::{Record, Logger};
+use lightning::util::logger::{Record, Logger, Level};
 
 #[pyclass]
+#[text_signature = "(logger, /)"]
+/// Logger interface. The constructor requires a class implementing, at least, the ``log(message: str, level: str)`` method.
 pub struct LDKLogger {
     inner: Py<PyAny>,
 }
@@ -15,21 +17,51 @@ impl LDKLogger {
         LDKLogger{inner: logger}
     }
 
-    // This may not be necessary given we don't need to log from python using the wrapper
-    // fn log(&self, record: String) {
-    //     // figure out how to deal with this
-    //     let message = format_args!("");
-    //     let r = Record::new(Level::Info, message, module_path!(), file!(), line!());
-    //     Logger::log(self, &r)
-    // }
+    #[text_signature = "($self, record, level)"]
+    fn log(&self, record: String, level: String) {
+        Python::with_gil(|py| {
+            let py_logger = self.inner.as_ref(py);
+            let message = format!("{}", record);
+            match py_logger.call_method("log", (message, level,), None){
+                Ok(_) => (),
+                Err(error) => error.print(py)
+            };
+        })
+    }
+
+
+    #[text_signature = "($self, record)"]
+    fn error(&self, record: String) {
+        self.log(record, Level::Error.to_string())
+    }
+
+    #[text_signature = "($self, record)"]
+    fn warn(&self, record: String) {
+        self.log(record, Level::Warn.to_string())
+    }
+
+    #[text_signature = "($self, record)"]
+    fn info(&self, record: String) {
+        self.log(record, Level::Info.to_string())
+    }
+
+    #[text_signature = "($self, record)"]
+    fn debug(&self, record: String) {
+        self.log(record, Level::Debug.to_string())
+    }
+
+    #[text_signature = "($self, record)"]
+    fn trace(&self, record: String) {
+        self.log(record, Level::Trace.to_string())
+    }
 }
 
 impl Logger for LDKLogger {
     fn log(&self, record: &Record) {
         Python::with_gil(|py| {
             let py_logger = self.inner.as_ref(py);
-            let message = format!("{:<5} [{} : {}, {}] {}", record.level.to_string(), record.module_path, record.file, record.line, record.args);
-            match py_logger.call_method("log", (message,), None){
+            let message = format!("{}", record.args);
+            match py_logger.call_method("log", (message, record.level.to_string()), None){
                 Ok(_) => (),
                 Err(error) => error.print(py)
             };
@@ -39,6 +71,7 @@ impl Logger for LDKLogger {
 
 
 #[pymodule]
+/// Loggin library for LDK.
 fn logger(_: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<LDKLogger>()?;
     Ok(())
