1c23260b9e8ff72da12c2b309345ccc2a5fa10dd
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2020-11-23 20:56:41 +0100

    Fixes setup to include all modules

--- ldk_python/__init__.py
+++
@@ -1,1 +0,0 @@
-from .ldk_python import *
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
+package_name = "ldk_python"
+extension_names = ["primitives", "logger", "chaininterface"]
+extensions = [RustExtension(f"{package_name}.{e_name}", binding=Binding.PyO3) for e_name in extension_names]
+
 setup(
     name="ldk-python",
     version="1.0",
-    rust_extensions=[RustExtension("ldk_python.ldk_python", binding=Binding.PyO3)],
+    rust_extensions=extensions,
     packages=["ldk_python"],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
