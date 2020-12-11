b2dd43c4f444fafeb02728cc33a2eff7a29945b5
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2020-12-11 17:16:39 +0100

    Adds a wrapper to CM<InMemoryChannelKeys> and MonitorUpdateErr
    Needs fix. Clone cannot be derived for CM<InMemoryChannelKeys>

--- src/chain/channelmonitor.rs
+++ src/chain/channelmonitor.rs
@@ -1,14 +1,44 @@
 use pyo3::create_exception;
 use pyo3::prelude::*;
 
+use crate::chain::chaininterface::{PyBroadcasterInterface, PyFeeEstimator};
+use crate::logger::LDKLogger;
+
 use lightning::chain::channelmonitor::{ChannelMonitor, ChannelMonitorUpdate, MonitorEvent};
 use lightning::chain::keysinterface::InMemoryChannelKeys;
 
 #[pyclass(name=InMemoryKeysChannelMonitor)]
-//#[derive(Clone)]
+#[derive(Clone)]
 pub struct PyInMemoryKeysChannelMonitor {
     inner: ChannelMonitor<InMemoryChannelKeys>,
 }
+
+#[pymethods]
+impl PyInMemoryKeysChannelMonitor {
+    fn update_monitor(
+        &mut self,
+        updates: PyChannelMonitorUpdate,
+        broadcaster: PyBroadcasterInterface,
+        fee_estimator: PyFeeEstimator,
+        logger: LDKLogger,
+    ) -> PyResult<()> {
+        match self.inner.update_monitor(
+            &updates.inner,
+            &Box::new(broadcaster),
+            &Box::new(fee_estimator),
+            &Box::new(logger),
+        ) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(MonitorUpdateErr::new_err(e.0)),
+        }
+    }
+}
+
+create_exception!(
+    channelmonitor,
+    MonitorUpdateErr,
+    pyo3::exceptions::PyException
+);
 
 #[pyclass(name=ChannelMonitorUpdate)]
 #[derive(Clone)]
