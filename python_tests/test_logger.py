bd34831b89d27b5386881f9f097dcb0159c84058
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2020-12-18 15:15:27 +0100

    Adds black formatting to Python files

--- python_tests/test_logger.py
+++ python_tests/test_logger.py
@@ -4,28 +4,31 @@ from conftest import Empty
 from ldk_python.logger import LDKLogger
 
 
-class Logger():
+class Logger:
     def log(self, message, level):
         print(f"[{level}]: {message}")
 
     def info(self, message):
         self.log(message, "INFO")
-        
+
     def warn(self, message):
         self.log(message, "WARN")
-        
+
     def error(self, message):
         self.log(message, "ERROR")
-        
+
     def debug(self, message):
         self.log(message, "DEBUG")
+
 
 inner_logger = Logger()
 ldk_logger = LDKLogger(inner_logger)
 
+
 def test_wrong_logger():
     with pytest.raises(TypeError, match="Not all required methods are implemented"):
         LDKLogger(Empty())
+
 
 def test_logger_log(capfd):
     inner_logger.log("This is a custom log", "CUSTOM_LEVEL")
@@ -35,6 +38,7 @@ def test_logger_log(capfd):
     ldk_out, _ = capfd.readouterr()
 
     assert inner_out == ldk_out
+
 
 def test_logger_info(capfd):
     message = "This is an info message"
@@ -46,6 +50,7 @@ def test_logger_info(capfd):
 
     assert inner_out == ldk_out
 
+
 def test_logger_warn(capfd):
     message = "This is a warning"
     inner_logger.warn(message)
@@ -55,6 +60,7 @@ def test_logger_warn(capfd):
     ldk_out, _ = capfd.readouterr()
 
     assert inner_out == ldk_out
+
 
 def test_logger_error(capfd):
     message = "This is an error"
@@ -66,6 +72,7 @@ def test_logger_error(capfd):
 
     assert inner_out == ldk_out
 
+
 def test_logger_debug(capfd):
     message = "This is a debug message"
     inner_logger.debug(message)
@@ -75,4 +82,3 @@ def test_logger_debug(capfd):
     ldk_out, _ = capfd.readouterr()
 
     assert inner_out == ldk_out
-
