354a391122159f0ab4ebc359210a081d65d34351
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2021-01-12 19:45:31 +0100

    Adds missing imports to Python Package

--- ldk_python/chain/channelmonitor/__init__.py
+++ ldk_python/chain/channelmonitor/__init__.py
@@ -1,3 +1,4 @@
 from .. import channelmonitor
 
 TemporaryChannelMonitorUpdateErr = channelmonitor.TemporaryChannelMonitorUpdateErr
+PermanentChannelMonitorUpdateErr = channelmonitor.PermanentChannelMonitorUpdateErr
--- ldk_python/ln/channelmanager/__init__.py
+++ ldk_python/ln/channelmanager/__init__.py
@@ -1,4 +1,15 @@
 from .. import channelmanager
 
+PaymentSendFailure = channelmanager.PaymentSendFailure
+ParameterError = channelmanager.ParameterError
+PathParameterError = channelmanager.PathParameterError
+AllFailedRetrySafe = channelmanager.AllFailedRetrySafe
+PartialFailure = channelmanager.PartialFailure
 
+
+PaymentPreimage = channelmanager.PaymentPreimage
+PaymentSecret = channelmanager.PaymentSecret
 PaymentHash = channelmanager.PaymentHash
+ChannelDetails = channelmanager.ChannelDetails
+ChannelManager = channelmanager.ChannelManager
+
